9cdbe230017d27644b99da01e3862085af083832
ramesh8850 <rameshgoud8850@gmail.com>
2025-02-19 13:23:46 +0530

    removed cors code

--- Bookmark/server.js
+++ Bookmark/server.js
@@ -8,24 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://bookmark-manager2.vercel.app",
-  "https://frontend-bookmark-manager.vercel.app"
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error("Not allowed by CORS")); // Block the request
-    }
-  },
-  methods: "GET, POST, PUT, DELETE",
-  credentials: true
-}));
-
+app.use(cors());
 
 app.use(express.json()); // For parsing JSON requests
 
